59c23aed53f9106a4d3e4b27572fc8b6ed08f7e5
JJerjes <juniormariluz1@gmail.com>
2024-12-11 00:32:47 -0500

    update

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuButton = document.querySelector('#menuButton');
+    const nav = document.querySelector('.navbar');
+
+    menuButton.addEventListener('click', () => {
+    nav.classList.toggle('open');
+    menuButton.classList.toggle('open')
+});
+})
